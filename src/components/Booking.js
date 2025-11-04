b8c11f69daebe7a2a9597c9d3477de32adc2e184
Rajakumar <Rajakumar040391@gmail.com>
2025-11-04 18:06:13 +0530

    final build

---
+++ src/components/Booking.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import BookingForm from "./BookingForm";
+
+const Booking = (props) => {
+
+
+
+    return (
+
+        <BookingForm availableTimes={props.availableTimes} dispatch={props.dispatch} submitForm={props.submitForm} />
+    )
+}
+
+export default Booking;
